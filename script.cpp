abc9a21707fc796166db3d1b7407617133e1438e
infolii <invateb@bk.ru>
2024-09-23 15:33:17 +0300

    Create script.cpp

---
+++ script.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <time.h>
+
+
+
+
+int main()
+{
+	time_t start, end;
+	time(&start);
+	int n = 0;
+	std::vector<int> v1;
+	while (n < 5000)
+	{
+		v1.push_back(n);
+		std::cout << n;
+		n += 2;
+
+	}
+	std::cout << std::endl;
+	time(&end);
+	double seconds = difftime(end, start);
+	printf("The time: %f seconds\n", seconds);
+
+}
